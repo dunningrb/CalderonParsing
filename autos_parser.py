bdaec1c2873e4540842380da1fb383627e27fc8c
dunningrb <dunningrb@gmail.com>
2019-02-06 08:46:03 -0500

    Add PERSONAS parsing.

--- autos_parser.py
+++ autos_parser.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
+
 """
 autos_parser
 
 Rodney Dunning
 
-Parses a single auto sacrementales in text format.
+Parses a single auto sacramentale in text format.
 
 """
 
@@ -14,10 +16,13 @@ import collections
 print('*** Starting. ***')
 
 LINES = {}
+PERSONAS = {}
 REMOVE_DIGITS = str.maketrans('', '', digits)
 PAGE_HEADER_SKIP_LINES = 4
 PAGE_HEADER_TOKENS = ['EL JARDÍN DE FALERINA', 'CALDERÓN DE LA BARCA']
 HTML_LINK_TOKENS = ['ANTERIOR', 'INICIO']
+CHARACTER_HEADING_TOKENS = ['PERSONAS',]
+
 
 def consume(iterator, n):
     "Advance the iterator n-steps ahead. If n is none, consume entirely."
@@ -27,6 +32,7 @@ def consume(iterator, n):
         collections.deque(iterator, maxlen=0)
     else:
         next(islice(iterator, n, n+1), None)
+
 
 print('Opening the file for reading.')
 
@@ -41,7 +47,26 @@ print('Finished reading the file into the lines dictionary.')
 print('Flagging page header lines in the dictionary.')
 
 lines_iterator = iter(LINES.items())
+personas_tokens_init = []
+personas_tokens_final = []
+personas_started = False
+personas_finished = False
+blank_line_counter = 0
+
 for line_number, line_data in lines_iterator:
+
+    if not personas_started and not personas_finished:
+         personas_started = any(token in line_data['text'] for token in CHARACTER_HEADING_TOKENS)
+
+    if personas_started and not personas_finished:
+        print(f'line_number: {line_number}; line_data[\'text\']: {line_data["text"].strip()}')
+        if not line_data['text'].strip():
+            blank_line_counter += 1
+            personas_finished = blank_line_counter == 2
+            print(f'\tblank_line_counter: {blank_line_counter}')
+        else:
+            personas_tokens_init.append(line_data['text'].strip().split('.'))
+
     if any(token in line_data['text'] for token in PAGE_HEADER_TOKENS):
         for i in range(PAGE_HEADER_SKIP_LINES):
             LINES[line_number+i]['keep'] = False
@@ -51,11 +76,23 @@ for line_number, line_data in lines_iterator:
     if any(token in line_data['text'].upper() for token in HTML_LINK_TOKENS):
         LINES[line_number]['keep'] = False
 
+for token in personas_tokens_init:
+    for sub_token in token:
+        if sub_token:
+            if ',' in sub_token:
+                sub_split_tokens = sub_token.split(',')
+                for sub_split_token in sub_split_tokens:
+                    personas_tokens_final.append(sub_split_token.strip())
+            else:
+                personas_tokens_final.append(sub_token.strip())
+
+print(f'PERSONAS: {personas_tokens_final}')
+
 print(f'Finished flagging page header lines in the dictionary.')
 
 print('Writing all lines not flagged into the output file.')
 
-with open('./el-jardin-output-yes-consume-5.txt', 'w') as f:
+with open('./el-jardin-output-yes-consume-6.txt', 'w') as f:
     for line_number, line_data in LINES.items():
         if line_data['keep']:
             f.write(line_data['text'].translate(REMOVE_DIGITS))
